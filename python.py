f7502020e782045067979a08c09fe0dc1d1d3554
Naoki-T-programming <programming.njack@gamil.com>
2022-03-05 12:44:30 +0900

    master

--- python.py
+++ python.py
@@ -1,1 +1,41 @@
-print("hello world")
+for i in range(100):
+    print("Hello, world!")
+
+import math 
+l = 4
+w = 10
+d = math.sqrt(l**2 + w**2)
+print(d) 
+
+print("""これは、とてもとても長い行の複数行のコードです。""")
+print\
+("これはとてもとても長い行の複数行のコードです。")
+
+x = 10
+x = x + 1
+print(x)
+x += 1
+print(x)
+
+x = "今夜は"
+y = "月が"
+z = "キレイですね"
+print(x)
+print(y)
+print(z)
+
+num = 17
+if num < 10:
+    print("蒟蒻")
+else:
+    print("蒟蒻でない")
+
+if num <= 10:
+    print("蒟蒻のよう")
+elif 10 < num <= 25:
+    print("ほぼ蒟蒻")
+else:
+    print("蒟蒻") 
+
+print(35 % 4)
+print(35 / 4)
